edd0fe7ee099896721964bf81e062eced43759a8
up1 <somkiat.p@gmail.com>
2019-04-02 12:01:34 +0700

    Add dashboard of hystrix

--- catalog/src/main/java/com/example/catalog/CatalogApplication.java
+++ catalog/src/main/java/com/example/catalog/CatalogApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
 public class CatalogApplication {
 
 	public static void main(String[] args) {
